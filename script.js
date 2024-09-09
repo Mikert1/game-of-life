901a66d423c5503cbd2afb788dd3e27d726cd207
M Balvert <mikebalvert@icloud.com>
2024-09-09 14:54:11 +0200

    Grid system #3

---
+++ script.js
@@ -0,0 +1,11 @@
+let game = document.getElementById('game');
+
+for (let i = 0; i < 30; i++) {
+    let row = document.createElement('div');
+    for (let j = 0; j < 15; j++) {
+        let div = document.createElement('div');
+        div.className = 'square';
+        row.appendChild(div);
+    }
+    game.appendChild(row);
+}
