063150a2d0c280ced5feffe80115e5dedefd16bf
M Balvert <mikebalvert@icloud.com>
2024-09-09 15:01:50 +0200

    clickable blocks #10

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ for (let i = 0; i < 30; i++) {
     for (let j = 0; j < 15; j++) {
         let div = document.createElement('div');
         div.className = 'square';
+        div.addEventListener('click', function() {
+            div.classList.toggle('active');
+        });
         row.appendChild(div);
     }
     game.appendChild(row);
